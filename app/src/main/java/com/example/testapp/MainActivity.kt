35e7264696747c8df8bafb389527b51324fbfbc5
Tirlim6om6om <57911899+Tirlim6om6om@users.noreply.github.com>
2024-03-03 21:37:19 +0300

    Initial commit

---
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.testapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+    }
+}
