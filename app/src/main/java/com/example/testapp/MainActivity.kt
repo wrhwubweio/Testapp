d302bbf621dc0f08858f43a356f5e7f141ea7bbb
Tirlim6om6om <57911899+Tirlim6om6om@users.noreply.github.com>
2024-03-03 22:16:57 +0300

    fx

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.testapp
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
+
+        if(savedInstanceState == null){
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_layout, Login())
+                .commit()
+        }
     }
 }
